8e7958aeb04d08a5282faf827138063a368baba3
Himanshi Jangid <himanshijangid444@gmail.com>
2023-02-09 16:08:01 +0530

    files added

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function changeColors() {
+    var hex_numbers=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+    
+    var hexcode='';
+    
+    for(i=0; i<6 ; i++){
+    
+        var random_index=Math.floor(Math.random()*hex_numbers.length);
+    
+        hexcode += hex_numbers[random_index]
+    }
+        document.getElementById("hex-code").innerHTML=hexcode;
+    
+        document.getElementsByClassName("container")[0].style.background="#"+hexcode
+    }
+    
